491e506301af1bfa3b6fc74655b69cbfe2e50576
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-07-17 01:24:56 +0900

    create animals#new,create

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,22 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+  def new
+    @animal = Animal.new
+  end
+  
+  def create
+    if Animal.create!(animal_params)
+      redirect_to animals_path
+    else
+      reder :new
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name)
+  end
 end
